0fbafbcdb43e938a5bcf64cc3a00723aa6e2d0a4
Anton Larsson <ante9311@gmail.com>
2015-12-01 15:22:49 +0100

    Added some test functions, used to check which way the heap grows.

--- heap.c
+++ heap.c
@@ -1,12 +1,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <stdint.h>
 
 struct heap_side {
   int size;
-  void* start;
-  void* first_free;
+  uintptr_t start;
+  uintptr_t first_free;
 };
 
 typedef struct heap_side heap_side;
@@ -21,19 +22,19 @@ struct heap {
 
 typedef struct heap heap;
 
-heap* new_heap(int sizet){
+heap* new_heap(int size){
   heap* temp_heap = (heap*)malloc(sizeof(heap));;
-  temp_heap->size = sizet;
+  temp_heap->size = size;
   temp_heap->active_side = true;
 
   temp_heap->left_side = (heap_side*)malloc(sizeof(heap_side));
   temp_heap->right_side = (heap_side*)malloc(sizeof(heap_side));
 
-  temp_heap->left_side->size = sizet;
-  temp_heap->right_side->size = sizet;
+  temp_heap->left_side->size = size;
+  temp_heap->right_side->size = size;
 
-  temp_heap->left_side->start = (int*)malloc(sizeof(int)*sizet);
-  temp_heap->right_side->start = (int*)malloc(sizeof(int)*sizet);
+  temp_heap->left_side->start = (uintptr_t)malloc(sizeof(int)*size);
+  temp_heap->right_side->start = (uintptr_t)malloc(sizeof(int)*size);
 
   temp_heap->left_side->first_free = temp_heap->left_side->start;
   temp_heap->right_side->first_free = temp_heap->right_side->start;
@@ -45,7 +46,54 @@ int get_size(heap* heap){
   return heap->size;
 }
 
+uint32_t get_first(heap* heap){
+  if (heap->active_side){
+    return (heap->left_side->first_free);
+  }
+  return 0;
+}
+
+bool write_to_left(heap* heap, int value){
+  int* first_free = (int*)heap->left_side->first_free;
+  *first_free = value;
+  heap->left_side->first_free = (heap->left_side->first_free) + sizeof(int);
+  return true;
+}
+
+bool write_to_heap(heap* heap, int value){
+  if (heap->active_side){
+    return write_to_left(heap, value);
+  }
+  else {
+    return false;
+  }
+}
+
+void print_heap(heap* heap){ /*Mainly a test function, will never be used in
+			       testing*/
+  if (heap->active_side){
+    uintptr_t start = heap->left_side->start;
+    uintptr_t end = heap->left_side->first_free;
+
+    
+    
+    int i = 0;
+    for (; start < end; start = start + sizeof(int)) {
+      printf("\n%d: %d", i++, *((int*)start));
+    }
+  }
+}
+
 int main (int argc, char* argv[]){
   heap* heap_test = new_heap(20);
   printf("Size: %d", get_size(heap_test));
+  
+  write_to_heap(heap_test, 99);
+  write_to_heap(heap_test, 98);
+  print_heap(heap_test);
+  //int* test_int = (int*)malloc(sizeof(int));
+  //*test_int = 99;
+  //void* first_free = (void*)get_first(heap_test);
+  //memcpy(first_free, test_int, sizeof(int));
+  //printf("\nInt is: %d", *((int*)first_free));
 }
