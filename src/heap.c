9cc11c2b55893fdef2ab3b7e4ad20ad591eb68c5
Anton Larsson <ante9311@gmail.com>
2015-12-08 13:01:59 +0100

    Can't remember what I did, forgot to commit last change

--- src/heap.c
+++ src/heap.c
@@ -1,8 +1,12 @@
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdbool.h> //For returns of functions.
+#include <stdlib.h> 
+#include <stdio.h> //For error checking
 #include <string.h>
 #include <stdint.h>
+#include <math.h> //For squareing in bit operations
+
+#define INTSIZE sizeof(int)
+#define PTRSIZE sizeof(uintptr_t)
 
 struct heap_side {
   int size;
@@ -31,20 +35,17 @@ heap* new_heap(int size){
   temp_heap->size = size;
   temp_heap->active_side = true;
 
-  int intsize = sizeof(int);
-  int ptrsize = sizeof(uintptr_t);
-
   heap_side* leftside = (heap_side*)malloc(sizeof(heap_side));
   heap_side* rightside = (heap_side*)malloc(sizeof(heap_side));
   
   leftside->size = size;
   rightside->size = size;
 
-  leftside->start = (uintptr_t)malloc( (intsize + ptrsize) * size );
-  rightside->start = (uintptr_t)malloc(sizeof(int)*size);
+  leftside->start = (uintptr_t)malloc( (INTSIZE + PTRSIZE) * size );
+  rightside->start = (uintptr_t)malloc( (INTSIZE + PTRSIZE) * size);
 
-  leftside->last_block = (leftside->start + ((intsize + ptrsize) * size) );
-  rightside->last_block = (rightside->start + ((intsize + ptrsize) *size));
+  leftside->last_block = (leftside->start + ((INTSIZE + PTRSIZE) * size) );
+  rightside->last_block = (rightside->start + ((INTSIZE + PTRSIZE) *size));
 
   leftside->first_free = leftside->start;
   rightside->first_free = rightside->start;
@@ -66,19 +67,52 @@ uint32_t get_first(heap* heap){
   return 0;
 }
 
-void* read_formatstring(char formatstring[]){
-  return NULL;
+uintptr_t set_header_size(char formatstring[]){
+  return 0;
+}
+
+int int_pow (int base, int exponent){
+  if (exponent == 0) return 1;
+
+  int result = base;
+  for (int i = 1; i < exponent; i++){
+    result = result * base;
+  }
+  return result;
+}
+
+uintptr_t read_formatstring(char formatstring[]){
+  size_t len = strlen( formatstring );
+  uintptr_t formatbinary = 0;
+  if ( len > ( (PTRSIZE * 8) - 3)  ){
+    set_header_size( formatstring );
+  }
+  else {
+    printf("\n");
+    for (int i = (len - 1); i >= 0; i--) {
+      printf("Character %d: %c\n", i, formatstring[i]);
+      if ( formatstring[i] == '*' ){ //If pointer, set bit to 1.
+	formatbinary = ( formatbinary | int_pow(2, i) ); //
+      }
+      // Else keep as 0 because databit should be 0;
+    }
+  }
+  printf("formatbinary: %d\n", formatbinary);
+  formatbinary = formatbinary << 2; /* Shift two steps left so we have room
+				       for the instruction bits */
+  return formatbinary;
 }
 
 bool write_to_side(heap_side* heapside, int value, char formatstring[]){
-  //void* header = read_formatstring(formatstring);
+  uintptr_t header = read_formatstring(formatstring);
+  if ( header != 0 ) printf("Header: %d\n", header);
   int* first_free = (int*)heapside->first_free;
   if (heapside->first_free >= heapside->last_block ||
       heapside->first_free < heapside->start) {
     return false;
   }
   *first_free = value;
-  heapside->first_free = (heapside->first_free)+sizeof(int)+sizeof(uintptr_t);
+  heapside->first_free = (heapside->first_free) + INTSIZE + PTRSIZE;
   return true;
 }
 
@@ -90,7 +124,7 @@ bool write_to_heap(heap* heap, int value, char formatstring[]){
     return write_to_side(heap->right_side, value, formatstring);
   }
   return false;
-}
+} 
 
 bool change_to_right(heap* head){
   return false;
@@ -116,9 +150,9 @@ void print_heap(heap* heap){ /*Mainly a test function,
     
     
     int i = 0;
-    printf("\n%d sizeof(int). %d sizeof(void*)\n", sizeof(int),
-	   sizeof(end));
-    for (; start < end; start = start + sizeof(int) + sizeof(uintptr_t)) {
+    printf("\n%d sizeof(int). %d sizeof(uintptr_t)\n", INTSIZE,
+	   PTRSIZE);
+    for (; start < end; start = start + INTSIZE + PTRSIZE) {
       printf("\n%d: %d", i++, *((int*)start));
     }
     printf("\nPrinted %d objects\n\n", i);
@@ -128,7 +162,7 @@ void print_heap(heap* heap){ /*Mainly a test function,
 int main (int argc, char* argv[]){
   heap* heap_test = new_heap(20);
   //printf("Size: %d\n", get_size(heap_test));
-  char test[] = "test";
+  char test[] = "**i*i**iiii*";
   for (int i = 0; i < 25; i++) {
     if (!write_to_heap(heap_test, (99)-i, test) ) {
       //printf("%d: Heap is full!\n", i);
