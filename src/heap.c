c104ff394a32f6f45d0503a916b72f6a8fa32087
Gustav <gustav.rosn@gmail.com>
2015-12-17 12:34:03 +0100

    heap_char_to_size()

--- src/heap.c
+++ src/heap.c
@@ -1,20 +1,43 @@
 #include <stdlib.h>
 #include "heap.h"
+#include <stdbool.h>
 
 typedef struct heap_side_t heap_side;
 typedef struct heap_t heap;
 
 struct heap_side_t{
-  int a;
-  int b;
+  
 };
 
 struct heap_t{
   heap_side* a;
   heap_side* b;
-
+  bool active;
 };
+
+int heap_char_to_size(char);
 
 heap *new_heap(){
   return (heap*) malloc(sizeof(heap));
+}
+
+void heap_alloc(char *s){
+  int i = heap_char_to_size(s[0]);
+  malloc(i);
+}
+
+int heap_char_to_size(char a){
+  switch (a){
+  case '*':
+    return 3;
+    break;
+
+  case 'i':
+    return sizeof(int);
+    break;
+    
+  default:
+    return 2;
+	  
+  }
 }
